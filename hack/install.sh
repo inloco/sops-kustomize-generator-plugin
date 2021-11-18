ab78128fc96495d38175270a254fe0507e69027d
Pedro Tôrres <t0rr3sp3dr0@gmail.com>
2021-11-18 19:13:14 -0300

    Update install.sh

--- hack/install.sh
+++ hack/install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-OS_ARCH=$(uname -m | sed 's/x86_64/amd64/g')
-OS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
-PLACEMENT=${XDG_CONFIG_HOME:-$HOME/.config}/kustomize/plugin/inloco.com.br/v1/sops
-PLUGIN=$PLACEMENT/SOPS
-RELEASE_URL=https://github.com/inloco/sops-kustomize-generator-plugin/releases/download/v0.0.0
+OS_ARCH="$(uname -m | sed 's/x86_64/amd64/g')"
+OS_NAME="$(uname -s | tr '[:upper:]' '[:lower:]')"
+PLACEMENT="${XDG_CONFIG_HOME:-${HOME}/.config}/kustomize/plugin/inloco.com.br/v1/sops"
+PLUGIN="${PLACEMENT}/SOPS"
+RELEASE_URL='https://github.com/inloco/sops-kustomize-generator-plugin/releases/download/v0.0.0'
 
-mkdir -p $PLACEMENT
-wget -O $PLUGIN ${RELEASE_URL}/plugin-${OS_NAME}-${OS_ARCH}
-chmod +x $PLUGIN
+mkdir -p "${PLACEMENT}"
+wget -O "${PLUGIN}" "${RELEASE_URL}/plugin-${OS_NAME}-${OS_ARCH}"
+chmod +x "${PLUGIN}"
