cb4b9214cdec2f6e138e3e59c175fb14bc6538f0
Miguel Oliveira <miguel.oliveira@incognia.com>
2026-06-11 15:06:07 -0300

    implement env-based decryption skip

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/getsops/sops/v3"
 	"github.com/getsops/sops/v3/aes"
 	sopsYAML "github.com/getsops/sops/v3/stores/yaml"
 	coreV1 "k8s.io/api/core/v1"
@@ -19,12 +20,35 @@ func main() {
 		log.Panic(filePath, ": ", err)
 	}
 
+	noDecryptEnv := os.Getenv("SOPS_NO_DECRYPT")
+
+	shouldDecrypt := noDecryptEnv != "1" && noDecryptEnv != "true"
+
+	if shouldDecrypt {
+		runDecrypt(filePath, encryptedData)
+	} else {
+		runNoDecrypt(filePath, encryptedData)
+	}
+}
+
+func runDecrypt(filePath string, encryptedData []byte) {
 	decryptedData, err := decrypt(encryptedData)
 	if err != nil {
 		log.Panic(filePath, ": ", err)
 	}
 
 	secret, err := makeSecret(decryptedData)
+	if err != nil {
+		log.Panic(filePath, ": ", err)
+	}
+
+	if _, err := os.Stdout.Write(secret); err != nil {
+		log.Panic(filePath, ": ", err)
+	}
+}
+
+func runNoDecrypt(filePath string, encryptedData []byte) {
+	secret, err := makeEncryptedSecret(encryptedData)
 	if err != nil {
 		log.Panic(filePath, ": ", err)
 	}
@@ -65,6 +89,76 @@ func makeSecret(data []byte) ([]byte, error) {
 		APIVersion: "v1",
 		Kind:       "Secret",
 	}
-	
+
 	return yaml.Marshal(secret)
+}
+
+func makeEncryptedSecret(data []byte) ([]byte, error) {
+	store := &sopsYAML.Store{}
+	tree, err := store.LoadEncryptedFile(data)
+	if err != nil {
+		return nil, err
+	}
+
+	secret := coreV1.Secret{}
+	secret.TypeMeta = metaV1.TypeMeta{
+		APIVersion: "v1",
+		Kind:       "Secret",
+	}
+
+	secret.Data = getDataNoDecrypt(tree.Branches)
+	secret.StringData = getStringDataNoDecrypt(tree.Branches)
+
+	metadata := getMetadataNoDecrypt(tree.Branches)
+	secret.ObjectMeta.Name = metadata["name"]
+	secret.ObjectMeta.Namespace = metadata["namespace"]
+
+	return yaml.Marshal(secret)
+}
+
+func getDataNoDecrypt(branches sops.TreeBranches) map[string][]byte {
+	for _, item := range branches[0] {
+		if item.Key == "data" {
+			var result = make(map[string][]byte)
+			dataFields := item.Value.(sops.TreeBranch)
+			for _, df := range dataFields {
+				result[df.Key.(string)] = []byte("no-decrypt")
+			}
+			return result
+		}
+	}
+	return nil
+}
+
+func getStringDataNoDecrypt(branches sops.TreeBranches) map[string]string {
+	for _, item := range branches[0] {
+		if item.Key == "stringData" {
+			var result = make(map[string]string)
+			stringData := item.Value.(sops.TreeBranch)
+			for _, df := range stringData {
+				result[df.Key.(string)] = "no-decrypt"
+			}
+			return result
+		}
+	}
+	return nil
+}
+
+func getMetadataNoDecrypt(branches sops.TreeBranches) map[string]string {
+	for _, item := range branches[0] {
+		if item.Key == "metadata" {
+			var result = make(map[string]string)
+
+			metadata := item.Value.(sops.TreeBranch)
+			for _, mdta := range metadata {
+				if mdta.Key.(string) == "name" {
+					result["name"] = mdta.Value.(string)
+				} else if mdta.Key.(string) == "namespace" {
+					result["namespace"] = mdta.Value.(string)
+				}
+			}
+			return result
+		}
+	}
+	return nil
 }
