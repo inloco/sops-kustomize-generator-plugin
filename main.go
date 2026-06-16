40fd33757b684509dc9bacd01bb4e0cf20cc918c
Miguel Oliveira <miguel.oliveira@incognia.com>
2026-06-16 15:02:02 -0300

    remove unecessary log

--- main.go
+++ main.go
@@ -25,7 +25,6 @@ func main() {
 
 	var secret []byte
 	if shouldDecrypt {
-		log.Println("Running with decryption")
 		secret, err = runDecrypt(encryptedData)
 	} else {
 		log.Println("[SOPS] Running in no-decrypt mode")
